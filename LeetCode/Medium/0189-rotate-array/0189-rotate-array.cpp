7b4d039790285a43201b2bf46979d1811af10ecb
ParkHongJ <qkrghdwns98@naver.com>
2026-05-05 21:31:54 +0900

    Time: 0 ms (100%), Space: 29.5 MB (65.54%) - LeetHub

--- LeetCode/Medium/0189-rotate-array/0189-rotate-array.cpp
+++ LeetCode/Medium/0189-rotate-array/0189-rotate-array.cpp
@@ -1,19 +1,10 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        list<int> l;
-        for (int i = 0; i < nums.size() - k; ++i)
-        {
-            l.push_back(nums[i]);
-        }
-        for (int i = 1; i <= k; ++i)
-        {
-            l.push_front(nums[nums.size() - i]);
-        }
-        for (int i = 0; i < nums.size(); ++i)
-        {
-            nums[i] = l.front();
-            l.pop_front();
-        }
+        k = k % nums.size();
+        
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
     }
 };
