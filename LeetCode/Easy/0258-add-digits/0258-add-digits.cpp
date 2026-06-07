31a5d1e9fc9a10df1b27f3aa95249208ce3eeeea
ParkHongJ <qkrghdwns98@naver.com>
2026-06-07 09:40:00 +0900

    Time: 0 ms (100%), Space: 9.5 MB (7.37%) - LeetHub

---
+++ LeetCode/Easy/0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        string s = to_string(num);
+        int answer = num;
+        while (s.length() > 1)
+        {
+            int temp = 0;
+            for (int i = 0; i < s.length(); ++i)
+            {
+                temp += s[i] -'0';
+            }
+            s = to_string(temp);
+            answer = temp;
+        }
+        return answer;
+    }
+};
